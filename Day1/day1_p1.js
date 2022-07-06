ffd0c16883e487e4ea7a95a0737c03386873fbbc
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-07-06 22:43:29 +0300

    working for testing

--- Day1/day1_p1.js
+++ Day1/day1_p1.js
@@ -1,10 +1,20 @@
 'use strict';
 
 const arr = [1721, 979, 366, 299, 675, 1456];
-for (let i = 0; i <= arr.length; i++) {
-  for (let j = i + 1; j <= arr.length; j++) {
-    if (arr[i] + arr[j] === 2020) {
-      console.log(arr[i] * arr[j]);
+let a = 0;
+let b = 0;
+const sum = () => {
+  for (let i = 0; i <= arr.length; i++) {
+    for (let j = i + 1; j <= arr.length; j++) {
+      if (arr[i] + arr[j] === 2020) {
+        a = arr[i];
+        b = arr[j];
+        console.log(a + b);
+      }
     }
   }
-}
+};
+
+const multy = (a, b) => a * b;
+
+module.exports = { sum, multy };
