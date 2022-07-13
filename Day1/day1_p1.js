3665eafc97c9a67c7af146ba546c563f57c2ac49
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-07-13 22:23:33 +0300

    finalization part 1 day 1

--- Day1/day1_p1.js
+++ Day1/day1_p1.js
@@ -1,20 +1,19 @@
 'use strict';
-
 const arr = [1721, 979, 366, 299, 675, 1456];
-let a = 0;
-let b = 0;
-const sum = () => {
+
+const searchTwoNumbers = arr => {
   for (let i = 0; i <= arr.length; i++) {
     for (let j = i + 1; j <= arr.length; j++) {
       if (arr[i] + arr[j] === 2020) {
-        a = arr[i];
-        b = arr[j];
-        console.log(a + b);
+        const a = arr[i];
+        const b = arr[j];
+        const array = [a, b];
+        return array;
       }
     }
   }
 };
 
-const multy = (a, b) => a * b;
+searchTwoNumbers(arr);
 
-module.exports = { sum, multy };
+module.exports = { searchTwoNumbers };
--- Day1/day1_p1.test.js
+++ Day1/day1_p1.test.js
@@ -2,11 +2,11 @@
 const data = require('./day1_p1');
 
 describe('Searching sum and multy from two entries', () => {
-  test('Two entries that sum to 2020', () => {
-    expect(data.sum).toBe(2020);
-  });
-
-  test('Two entries multiply them together', () => {
-    expect(data.multy).toBe(514579);
+  test('Two entries that sum to 2020 and get result multy it', () => {
+    const arr = [1721, 979, 366, 299, 675, 1456];
+    const result = data.searchTwoNumbers(arr);
+    expect(result).toHaveLength(2);
+    expect(result[0] + result[1]).toBe(2020);
+    expect(result[0] * result[1]).toBe(514579);
   });
 });
