c37cdb29ecef7d8d3dae30dbb3c5fb1c0649fa8f
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-07-18 07:55:40 +0300

    working at day2

--- Day2/day2.js
+++ Day2/day2.js
@@ -1,23 +1,31 @@
 'use strict';
 const fs = require('fs');
-const num = fs.readFileSync('./input_02.txt', 'utf-8');
+const num = fs.readFileSync(
+  '/Users/Elena/AdventOfCode/Day2/input_02.txt',
+  'utf-8'
+);
 const strings = num.slice(/\d+-\d+\s[a-z]:\s\w+/g);
 const str = strings.split('\n');
 
-for (let b = 0; b <= str.length - 1; b++) {
-  const elements = str[b].split(' ');
+const wrapper = (str) => {
+  let qty = 0;
+  for (let b = 0; b <= str.length - 1; b++) {
+    const elements = str[b].split(' ');
+    const numbers = elements[0].split('-').map(Number);
+    const letter = elements[1].slice(0, 1);
+    const pass = elements[2];
 
-  const numbers = elements[0].split('-').map(Number);
-  const letter = elements[1].slice(0, 1);
-  const pass = elements[2];
-
-  let sum = 0;
-  for (let i = 0; i <= pass.length - 1; i++) {
-    if (pass[i] === letter) {
-      sum++;
+    let checkLetterQty = 0;
+    for (let i = 0; i <= pass.length - 1; i++) {
+      if (pass[i] === letter) {
+        checkLetterQty++;
+      }
+    }
+    if (checkLetterQty >= numbers[0] && checkLetterQty <= numbers[1]) {
+      qty++;
     }
   }
-  if (sum >= numbers[0] && sum <= numbers[1]) {
-    console.log('Access code accepted!');
-  } else console.log('Access denied!');
-}
+  console.log(qty);
+};
+wrapper(str);
+module.exports = { wrapper };
