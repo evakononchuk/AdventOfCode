ce1a5e7849de40eb55b549c851768ca06ceffea2
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-08-01 13:25:18 +0300

    add day2.test.js

--- Day2/day2.test.js
+++ Day2/day2.test.js
@@ -1,15 +1,10 @@
 'use strict';
-const data = require('./day2.js');
-const fs = require('fs');
-const { describe } = require('yargs');
-const num = fs.readFileSync(
-  '/Users/Elena/AdventOfCode/Day2/input_02.txt',
-  'utf-8'
-);
+const wrap = require('./day2');
+const num = '1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc';
 const strings = num.slice(/\d+-\d+\s[a-z]:\s\w+/g);
 const str = strings.split('\n');
 
 test('Count quantity correct passwords', () => {
-  const result = data.wrapper(str);
-  expect(result).toBe(396);
+  const result = wrap.wrapper(str);
+  expect(result).toBe(2);
 });
