77115e7825845907d6e399167481d4bdd8c3b1d9
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-08-01 12:57:16 +0300

    change day2.js

--- Day2/day2.js
+++ Day2/day2.js
@@ -1,4 +1,5 @@
 'use strict';
+const wrap = require('./day2base');
 const fs = require('fs');
 const num = fs.readFileSync(
   '/Users/Elena/AdventOfCode/Day2/input_02.txt',
@@ -6,26 +7,6 @@ const num = fs.readFileSync(
 );
 const strings = num.slice(/\d+-\d+\s[a-z]:\s\w+/g);
 const str = strings.split('\n');
+const result = wrap.wrapper(str);
 
-const wrapper = (str) => {
-  let qty = 0;
-  for (let b = 0; b <= str.length - 1; b++) {
-    const elements = str[b].split(' ');
-    const numbers = elements[0].split('-').map(Number);
-    const letter = elements[1].slice(0, 1);
-    const pass = elements[2];
-
-    let checkLetterQty = 0;
-    for (let i = 0; i <= pass.length - 1; i++) {
-      if (pass[i] === letter) {
-        checkLetterQty++;
-      }
-    }
-    if (checkLetterQty >= numbers[0] && checkLetterQty <= numbers[1]) {
-      qty++;
-    }
-  }
-  console.log(qty);
-};
-wrapper(str);
-module.exports = { wrapper };
+//console.dir(result);
