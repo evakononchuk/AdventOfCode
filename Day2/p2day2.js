f54e2f68da05e5fa5441709c8016bdf5bab10e11
Elena Kononchuk <eva.kononchuk@gmail.com>
2022-09-06 17:01:33 +0300

    delete unnecessary

--- Day2/p2day2.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const wrap = require('./p2day2base');
-const fs = require('fs');
-const num = fs.readFileSync('/Users/Elena/AdventOfCode/sample2.txt', 'utf-8');
-const str = num.split('\n');
-const result = wrap.wrapper(str);
-//console.log(result);
